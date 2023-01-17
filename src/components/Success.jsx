91475f5cdf1807db977be3c710f35bbe117155cc
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-01-17 08:55:45 +0200

    created a new app

---
+++ src/components/Success.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Success = ({ count }) => {
+    return (
+        <div class="success-block">
+            <img src="/assets/success.svg" alt="Success" />
+            <h3>Успішно!</h3>
+            <p>Всім {count} користувачам надіслано запрошення.</p>
+            <button
+                onClick={() => window.location.reload()}
+                className="send-invite-btn"
+            >
+                Назад
+            </button>
+        </div>
+    );
+};
