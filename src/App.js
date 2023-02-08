0e5a0541321429a33e01c6f44b95fe029e953e99
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-02-08 06:42:30 +0200

    fix style

--- src/App.js
+++ src/App.js
@@ -1,60 +1,60 @@
-import React, { useState, useEffect } from "react";
-import "./index.scss";
-import { Success } from "./components/Success";
-import { Users } from "./components/users";
+import React, { useState, useEffect } from 'react';
+import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/users';
 
 function App() {
-    const [users, setUsers] = useState([]);
-    const [invites, setInvites] = useState([]);
-    const [isLoading, setLoading] = useState(true);
-    const [success, setSuccess] = useState(false);
-    const [searchValue, setSearchValue] = useState("");
+  const [users, setUsers] = useState([]);
+  const [invites, setInvites] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [success, setSuccess] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
 
-    useEffect(() => {
-        fetch("https://reqres.in/api/users")
-            .then((res) => res.json())
-            .then((json) => {
-                setUsers(json.data);
-            })
-            .catch((err) => {
-                console.log(err);
-                alert("Помилка при отриманні користувачів");
-            })
-            .finally(() => setLoading(false));
-    }, []);
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert('Помилка при отриманні користувачів');
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-    const onChangeSearchValue = (event) => {
-        setSearchValue(event.target.value);
-    };
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
 
-    const onClickInvite = (id) => {
-        if (invites.includes(id)) {
-            setInvites((prev) => prev.filter((_id) => _id !== id));
-        } else {
-            setInvites((prev) => [...prev, id]);
-        }
-    };
-    const onClickSendInvites = () => {
-        setSuccess(true);
-    };
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  };
 
-    return (
-        <div className="App">
-            {success ? (
-                <Success count={invites.length} />
-            ) : (
-                <Users
-                    onChangeSearchValue={onChangeSearchValue}
-                    searchValue={searchValue}
-                    items={users}
-                    isLoading={isLoading}
-                    invites={invites}
-                    onClickInvite={onClickInvite}
-                    onClickSendInvites={onClickSendInvites}
-                />
-            )}
-        </div>
-    );
+  return (
+    <div className="App">
+      {success ? (
+        <Success count={invites.length} />
+      ) : (
+        <Users
+          onChangeSearchValue={onChangeSearchValue}
+          searchValue={searchValue}
+          items={users}
+          isLoading={isLoading}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSendInvites={onClickSendInvites}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
